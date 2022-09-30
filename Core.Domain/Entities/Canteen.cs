a31823e40a66d336efdececc166c4ee75719d300
Calvin <cl.kleijn@student.avans.nl>
2022-09-30 18:49:10 +0200

    Add all entities and enums V1

---
+++ Core.Domain/Entities/Canteen.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Domain.Entities
+{
+    public class Canteen
+    {
+        public int CanteenId { get; set; }
+        [Required(ErrorMessage = "Stad is verplicht!")]
+        public Cities? City { get; set; }
+        [Required(ErrorMessage = "Locatie is verplicht!")]
+        public string? Location { get; set; }
+        [Required(ErrorMessage = "Aangeven ofdat een kantine warme maaltijden verkoopt is verplicht!")]
+        public bool? OfferingHotMeals { get; set; }
+    }
+}
---
+++ Core.Domain/Entities/Packet.cs
@@ -0,0 +1,26 @@
+﻿namespace Core.Domain.Entities
+{
+    public class Packet
+    {
+        public int PacketId { get; set; }
+        [Required(ErrorMessage = "Naam is verplicht!")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage = "Producten zijn verplicht!")]
+        public ICollection<Product>? Products { get; set; }
+        [Required(ErrorMessage = "Stad is verplicht!")]
+        public Cities? City { get; set; }
+        [Required(ErrorMessage = "Kantine is verplicht!")]
+        public Canteen? Canteen { get; set; }
+        [Required(ErrorMessage = "Ophaaldatum en tijdstip is verplicht!")]
+        public DateTime? PickUpDateTime { get; set; }
+        [Required(ErrorMessage = "Uiterlijke ophaal tijdstip is verplicht!")]
+        public TimeOnly? LatestPickUpTime { get; set; }
+        [Required(ErrorMessage = "Het aangeven van ofdat het pakket 18+ is is verplicht!")]
+        public bool? IsEightteenPlusPacket { get; set; }
+        [Required(ErrorMessage = "Prijs is verplicht!")]
+        public double? Price { get; set; }
+        [Required(ErrorMessage = "Maaltijd type is verplicht!")]
+        public MealTypes? MealType { get; set; }
+        public Student? ReservedBy { get; set; }
+    }
+}
