ae2d5bd5d6f45b837969fdbe06a9a0f8bcc665c8
Calvin <cl.kleijn@student.avans.nl>
2022-10-01 14:17:32 +0200

    Add all repositories interfaces V1 + Add AmountOfReports property to Student

---
+++ Core.DomainServices/Interfaces/IStudentRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.DomainServices.Interfaces
+{
+    public interface IStudentRepository
+    {
+        Task<Student> GetStudentByIdAsync(int StudentId);
+        Task CreateStudentAsync(Student student);
+        Task ReportStudentAsync(Student student);
+    }
+}
