28f129cc508c4c5b62762a2fb79bbdb7214cede9
Calvin <cl.kleijn@student.avans.nl>
2022-10-23 19:32:08 +0200

    Update Swagger version

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using WebAPI.GraphQL;
 using Infrastructure.Repositories;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,7 +15,11 @@ builder.Services.AddControllers();
 builder.Services.AddTransient<PacketRepository>();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(v =>
+{
+    v.SwaggerDoc("v1", new OpenApiInfo { Title = "TGTG-Avans REST API", Version = "v1" });
+});
+
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationConnectionString")));
 builder.Services.AddDbContext<SecurityDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SecurityConnectionString")));
@@ -35,9 +40,15 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
+
+app.UseSwagger();
+
+app.UseSwaggerUI(v =>
+{
+    v.SwaggerEndpoint("/swagger/v1/swagger.json", "TGTG-Avans REST API V1");
+});
 
 app.UseHttpsRedirection();
 
